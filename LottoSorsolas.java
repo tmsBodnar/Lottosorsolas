33cedd3ce5631f04e1088ca7a8ecb366713a4d83
Tms-bodnar <tms.bodnar@gmail.com>
2016-09-22 21:35:08 +0200

    Update LottoSorsolas.java
    Kommentelve

--- LottoSorsolas.java
+++ LottoSorsolas.java
@@ -13,10 +13,11 @@ import java.util.Scanner;
  * @author bodnart
  */
 public class LottoSorsolas {
-
+//itt kérjük be a játékos tippjeit
     public static int tipp(){
         Scanner sc = new Scanner(System.in);
         int tipp;
+//ebben a ciklusban vizsgáljuk, hogy 1-35 között van-e a tipp        
         do{
         tipp = sc.nextInt();
         if(tipp <= 0 || tipp > 35){
@@ -26,14 +27,14 @@ public class LottoSorsolas {
         System.out.print("");
         return tipp;
     }
-    
+//itt a sorsolás zajlik :-)    
     public static int sorsolas(){
         Random rnd = new Random();
         int sors = rnd.nextInt(34);
         System.out.print("");
         return sors;
     }
-    
+//itt megnézzük, van-e már ilyen tipp a tippek tömbben    
     public static boolean tippEllenor(int[] tippek, int tipp){
         boolean helyesTipp = true;
         for( int i = 0; i < tippek.length; i++){
@@ -43,10 +44,10 @@ public class LottoSorsolas {
             }
         return helyesTipp;
     }
-    
+// itt egy tömbbe helyezzük a találatokat    
     public static void talalat(int[] tippek, int[]sorsolas){
         int talalat = 0;
-        
+//először megszámoljuk, hány találata van a játékosnak        
         for (int i = 0; i < tippek.length; i++) {
             for(int szam : sorsolas){
                 if(tippek[i] == szam){
@@ -54,6 +55,7 @@ public class LottoSorsolas {
                 }
             }
         }
+//majd ezt az előbb megállapított méretű találatok tömbben elhelyezzük        
         int talalatok[] = new int[talalat];
         int x = 0;
         for (int i = 0; i < tippek.length; i++) {
@@ -64,21 +66,25 @@ public class LottoSorsolas {
                 }
             }
         }
+// ha volt találata, kiírjuk hogy mennyi és milyen számokat talált el        
         if ( talalat > 0){
             System.out.println(talalat + " találata volt: ");
             for(int szam:talalatok){
                 System.out.println(szam);
             }
+// ha nem volt elküldjük a fenébe             
         }else
-            System.out.println("Sajnos nem nyert!");
+            System.out.println("Lúzer!(Sajnos nem nyert!)");
     }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+//létrehozunk egy 7 elemű tömböt a tippeknek       
         int [] tippek = new int[7];
         int x = 0;
+//feltöltjük a tömböt a függvénnyel bekért és ellenőrzött számokkal        
         do{
         System.out.print("Az " + (x+1) + ". tippje a hétből:");    
         int jatekos = tipp();
@@ -90,14 +96,17 @@ public class LottoSorsolas {
         }
             
         }while( x < 7 );
+//A végén kiírjuk egyben a tippeket        
         System.out.print("A tippjei: ");
         for(int szam:tippek){
             System.out.print(szam + ", ");
             
         }
         System.out.println("");
+//létrehozunk egy 7 elemű tömböt a sorsolásnak        
         int[] sorsolas = new int[7];
         int y = 0;
+//feltöltjük a függvényektől bekért és ellenőrzött számokkal
         do{
         int sors = (sorsolas() + 1);            
         if (tippEllenor(sorsolas, sors)){
@@ -105,11 +114,13 @@ public class LottoSorsolas {
             y++;
             }
         }while (y < 7);
+// kiírjuk a nyerőszámokat
         System.out.print("A nyerőszámok: ");
         for(int szam:sorsolas){
             System.out.print(szam + ", ");
             }
         System.out.println("");
+//majd meghívjuk a talalat függvényt, ami kiírja a találatokat        
     talalat(tippek,sorsolas);
     }
     
